12146b89e12abb11d2ccf8fefeb227282e73543c
veda2326 <veda.d.26230504@gmail.com>
2026-04-06 14:26:18 +0530

    Update Suspicious keyword analysis.py

--- Suspicious keyword analysis.py
+++ Suspicious keyword analysis.py
@@ -1,13 +1,19 @@
 # List of suspicious phishing words
-
 suspicious_words = ['urgent', 'verify', 'password', 'account', 'bank', 'click', 'login', 'limited', 'suspended']
 
-# Count suspicious words in each email
 
+# Count suspicious words in each email
 df['suspicious_word_count'] = df['email_text'].apply(
     lambda x: sum(word in x.lower() for word in suspicious_words)
 )
 
-# Compare suspicious word count by email type
 
+# Compare suspicious word count by email type
 df.groupby('email_type')['suspicious_word_count'].mean()
+
+
+# Plot suspicious word count
+plt.figure(figsize=(8,5))
+sns.boxplot(x='email_type', y='suspicious_word_count', data=df)
+plt.title('Suspicious Word Count by Email Type')
+plt.show()
