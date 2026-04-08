1c5245bb32ed8973b271bee7bb6f4e196251605c
veda2326 <veda.d.26230504@gmail.com>
2026-04-08 12:00:16 +0530

    Update email text normalization.py

--- email text normalization.py
+++ email text normalization.py
@@ -1,3 +1,27 @@
+
+
+
+
+
+
+
+
+
+# Count URLs in each email
+df['url_count'] = df['email_text'].apply(
+    lambda x: len(re.findall(r'http[s]?://\\S+|www\\.\\S+', x))
+)
+
+# Compare URL count by email type
+df.groupby('email_type')['url_count'].mean()
+
+# Plot URL count by email type
+plt.figure(figsize=(8,5))
+sns.boxplot(x='email_type', y='url_count', data=df)
+plt.title('URL Count by Email Type')
+plt.show()
+
+
 # Count exclamation marks in emails
 df['exclamation_count'] = df['email_text'].apply(lambda x: x.count('!'))
 
